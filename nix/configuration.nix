a6ef850ad2c6e1c1a7f81c0df2c93058c4816b93
oolong <asrs.contact+dev@gmail.com>
2023-10-25 14:12:18 +0800

    changes: plymouth and lightdm work and are themed

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -16,12 +16,13 @@ in
   boot.initrd.systemd.enable = true; #authorize plymouth in stage 1
   boot.plymouth.enable = true;
   boot.plymouth.theme = "breeze";
-  #boot.kernelParams = ["quiet"];
+  boot.kernelParams = ["quiet"];
 
   # Setup keyfile
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
   };
+
 
   # Hostname
   networking.hostName = "koi"; # Define your hostname.
@@ -57,13 +58,18 @@ in
     LC_TIME = "en_HK.UTF-8";
   };
 
-  # Configure keymap in X11
+  # Configure X11
   services.xserver = {
-    enable = true;
-    windowManager.openbox.enable = true;
+    enable = true;                       # enable X11
+    windowManager.openbox.enable = true; # enable Openbox
     layout = "us";
     xkbVariant = "";
   };
+
+
+  # Configure LightDM themes
+  services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
+  services.xserver.displayManager.lightdm.greeters.gtk.theme.name = "Adwaita-dark";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."${myuser}" = {
@@ -89,15 +95,16 @@ in
     vim
     wget
     htop
-    powertop
+    powertop            # Intel Base power analyze
     sshpass
     python311
-    nixos-bgrt-plymouth # nice theme for plymouth
-    home-manager        # a Nix Dotfiles manager
-    pipewire            # to manage sound
-    alsa-utils          # to manage audio card
-    udisks              # to manage USB automount
+    nixos-bgrt-plymouth # nice theme for plymouth 
+    lightdm-gtk-greeter # nice theme for lightDM
+    home-manager        # a Nix Dotfiles manager for users
     light               # to manage brightness
+    alsa-utils          # to manage audio card
+    pipewire            # to manage sound
+    udisks              # to manage USB automount
     libvirt             # to manage VM
     qemu_full           # to have VM
     podman              # to have containers
@@ -136,13 +143,13 @@ in
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+    pulse.enable = true; # to support Pavucontrol GUI
+    jack.enable = true;  # to support DAW
   };
 
   # automount Device
-  services.udisks2.enable = true;
   services.gvfs.enable = true;
+  services.udisks2.enable = true;
 
   # activate libvirt
   virtualisation = {
