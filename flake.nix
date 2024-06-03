d9970d41d64a1fed1947946d7158814ba229a33e
aeliusrs <asrs.contact+dev@gmail.com>
2024-06-03 14:24:42 +0800

    trying to home-manager in flake...

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
     {
       nixosConfigurations = {
 
@@ -14,15 +18,17 @@
           system = "x86_64-linux";
           modules = [
             ./nix/work-laptop/configuration.nix
+
+             home-manager.nixosModules.home-manager {
+               home-manager.useGlobalPkgs = true;
+               home-manager.useUserPackages = true;
+               home-manager.users.aeliusrs = import ./nix/work-laptop/home.nix;
+             }
           ];
         };
 
-        # arrow = nixpkgs.lib.nixosSystem {
-          # system = "x86_64-linux";
-          # modules = [
-            # ./nix/work-laptop/configuration.nix
-          # ];
-        # };
+
+       # import other config here ...
 
       };
     };
