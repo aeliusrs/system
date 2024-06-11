47e4a9e7b59d22cacbce48bec47c711888a803ef
aeliusrs <asrs.contact+dev@gmail.com>
2024-06-11 11:30:19 +0800

    fix some latency on decryption

--- nix/work-laptop/hardware-configuration.nix
+++ nix/work-laptop/hardware-configuration.nix
@@ -6,7 +6,7 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "cryptd" "aesni_intel" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
